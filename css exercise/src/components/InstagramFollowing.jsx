e4fc6be6b142af73ac24ec4b6452a025d882a8f0
nishantattrey07 <nishantattrey@gmail.com>
2024-10-19 19:42:47 +0530

    Refactor tailwind-practise project configuration and add necessary files

---
+++ css exercise/src/components/InstagramFollowing.jsx
@@ -0,0 +1,15 @@
+
+export function InstaFollow() { 
+
+    return (
+    <div style={{ display: "block" }}>
+        <div style={{display:"inline-block", margin:"5px"}}>
+        <img src="/css exercise/src/assets/profile.jpg" alt="img" />
+        </div>
+        <div style={{ display: "inline-block" }}>
+            <p>username</p>
+            <p>name</p>
+        </div>
+        </div>
+    )
+}
---
+++ tailwind-practise/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3">
+        <div className="bg-red-500">
+          <p>This is Div1</p>
+        </div>
+        <div className="bg-green-500">
+          <p>This is Div2</p>
+        </div>
+        <div className="bg-yellow-500">
+          <p>This is Div3</p>
+        </div>
+        
+      </div>
+    </>
+  );
+}
+
+export default App
