63138f28491236cd368e80371f33ce3ca2ecc005
nishantattrey07 <nishantattrey@gmail.com>
2024-10-20 14:40:42 +0530

    Refactor tailwind-practise project configuration and add necessary files

--- tailwind-practise/src/App.jsx
+++ tailwind-practise/src/App.jsx
@@ -1,25 +1,39 @@
 
+import { RevenueCard } from './components/RevenueCard';
 import './App.css'
+import { Payout } from './components/Payout';
+import { AppBar } from './components/AppBar';
 
 function App() {
 
 
   return (
-    <>
-      <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3">
-        <div className="bg-red-500">
-          <p>This is Div1</p>
+    <div>
+      <header>
+        <AppBar />
+      </header>
+      <section className="flex flex-col gap-6">
+        <div className=" flex gap-5 flex-wrap">
+          <Payout
+            title={"Next Payout"}
+            amount={"2312.32"}
+            orders={"23"}
+            paymentTiming={"Today, 4:00PM"}
+          />
+          <RevenueCard
+            title="Amount Pending"
+            revenue="92,312.20"
+            orderCount={13}
+          />
+          <RevenueCard title="Amount Processed" revenue="1,92,312.20" />
+          {/* Add more RevenueCard components as needed */}
         </div>
-        <div className="bg-green-500">
-          <p>This is Div2</p>
-        </div>
-        <div className="bg-yellow-500">
-          <p>This is Div3</p>
-        </div>
-        
-      </div>
-    </>
+      </section>
+    </div>
   );
 }
 
 export default App
+
+
+// grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4
--- tailwind-practise/tailwind.config.js
+++ tailwind-practise/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          700: "#2a77bb"
+        }
+      }
+    },
   },
   plugins: [],
 }
