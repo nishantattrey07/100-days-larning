47d9e017206088bfc5aa95d8fa758e25472fc0cd
nishantattrey07 <nishantattrey@gmail.com>
2024-10-21 23:48:39 +0530

    Refactor package.json and package-lock.json to add lucide-react dependency

--- tailwind-practise/src/App.jsx
+++ tailwind-practise/src/App.jsx
@@ -1,41 +1,42 @@
-
-import { RevenueCard } from './components/RevenueCard';
-import './App.css'
-import { Payout } from './components/Payout';
-import { AppBar } from './components/AppBar';
-import { OverViewBar } from './components/OverViewBar';
+import { RevenueCard } from "./components/RevenueCard";
+import "./App.css";
+import { Payout } from "./components/Payout";
+import { AppBar } from "./components/AppBar";
+import { OverViewBar } from "./components/OverViewBar";
+import { SideBar } from "./components/SideBar";
 
 function App() {
-
-
   return (
-    <div>
-      <header>
-        <AppBar title={"Payouts"}/>
-      </header>
-      <section className="flex flex-col gap-6">
-        <OverViewBar title={"OverView"}/>
-        <div className=" flex gap-5 flex-wrap">
-          <Payout
-            title={"Next Payout"}
-            amount={"2312.32"}
-            orders={"23"}
-            paymentTiming={"Today, 4:00PM"}
-          />
-          <RevenueCard
-            title="Amount Pending"
-            revenue="92,312.20"
-            orderCount={13}
-          />
-          <RevenueCard title="Amount Processed" revenue="1,92,312.20" />
-          
-        </div>
-      </section>
+    <div className="flex flex-row justify-between">
+      <div className=" ">
+        <SideBar />
+      </div>
+      <div>
+        <header>
+          <AppBar title={"Payouts"} />
+        </header>
+      </div>
+      <div>
+        <section className="flex flex-col gap-6">
+          <OverViewBar title={"OverView"} />
+          <div className=" flex gap-5 flex-wrap">
+            <Payout
+              title={"Next Payout"}
+              amount={"2312.32"}
+              orders={"23"}
+              paymentTiming={"Today, 4:00PM"}
+            />
+            <RevenueCard
+              title="Amount Pending"
+              revenue="92,312.20"
+              orderCount={13}
+            />
+            <RevenueCard title="Amount Processed" revenue="1,92,312.20" />
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
 
-export default App
-
-
-// grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4
+export default App;
